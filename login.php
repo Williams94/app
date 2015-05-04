cd86b7bde12ec6f8165ca3a551c254fdb7ff7e82
Williams94 <r.b.williams94@gmail.com>
2015-05-03 20:15:10 -0400

    Small change

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-	$sqlconn = mysqli_connect('178.62.75.76', 'buildax', 'Skinks94','buildax') or die(mysqli_error());
+	$sqlconn = mysqli_connect('localhost', 'buildax', 'Skinks94','buildax') or die(mysqli_error());
 
 	$dataquery = mysqli_query($sqlconn, "SELECT * FROM users");
 
