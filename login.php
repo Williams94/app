ceca7b18ebdf361f7aa9e157c0a5e4a2598c9012
Ross Williams <r.b.williams94@gmail.com>
2015-05-04 01:11:05 +0100

    Fixed bug

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
 	$sqlconn = mysqli_connect('178.62.75.76', 'buildax', 'Skinks94','buildax') or die(mysqli_error());
 
-	$dataquery = mysqli_query("SELECT * FROM users");
+	$dataquery = mysqli_query($sqlconn, "SELECT * FROM users");
 
 	$arr = array();
 	while($r = mysqli_fetch_object($dataquery)){
